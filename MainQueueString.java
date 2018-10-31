daf891ae4fac6dc26304fd6396caa2d8279d0a81
RIFAL09 <43751389+RIFAL09@users.noreply.github.com>
2018-10-31 22:37:32 +0800

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,15 @@
+package TUGAS_ASD_3;
+import sun.misc.Queue;
+public class MainQueueString {
+
+	public static void main(String[] args) {
+		QueueString q = new QueueString();
+		q.cetak();
+		
+		q.insert("rifal");q.cetak();
+		q.insert("ifal");q.cetak();
+		q.insert("mail");q.cetak();
+		q.insert("ismail");q.cetak();
+
+	}
+}
